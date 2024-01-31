b7ae917d297098b6f3a3dcf548edb63bb4f9d595
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2024-01-31 11:28:50 -0800

    Commit message

--- tictactoe/EventLoop.java
+++ tictactoe/EventLoop.java
@@ -26,7 +26,8 @@ public class EventLoop {
                 state.setOName(ui.promptForName("O"));
                 state.setGameState(Constants.GET_X_MOVE);
 
-            } else if (gameState == Constants.GET_X_MOVE) {
+            } else if (gameState == Constants.GET_X_MOVE) { 
+                ui.printBoard(state);
                 row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
                 col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
                 if (ui.isLegalMove(state, row, col)) {
@@ -34,6 +35,7 @@ public class EventLoop {
                 }
 
             } else if (gameState == Constants.GET_O_MOVE) {
+                ui.printBoard(state);
                 row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
                 col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
                 if (ui.isLegalMove(state, row, col)) {
